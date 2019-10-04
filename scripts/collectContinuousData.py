edd688c483622ced8203fc6dee9e8d96c37dd80e
Nadine Chang <zdan430@gmail.com>
2019-10-04 13:18:12 -0400

    script ready for baseline collection

--- scripts/collectContinuousData.py
+++ scripts/collectContinuousData.py
@@ -18,6 +18,13 @@ def parse_args():
         type=str,
     )
     parser.add_argument(
+        "--type",
+        dest="type",
+        help="run type: base, collect",
+        default="base",
+        type=str,
+    )
+    parser.add_argument(
         "--batchnum",
         dest="batchnum",
         help="current data batch number",
@@ -28,7 +35,7 @@ def parse_args():
         "--tol",
         dest="tol",
         help="difference tolerance",
-        default=0.2,
+        default=0.22,
         type=float,
     )
 
@@ -38,109 +45,125 @@ def parse_args():
 
 args = parse_args()
 
-date_time = datetime.now().strftime("%Y-%m-%d %H-%M")
-filename = date_time + " 5X Data.pkl"
-serZero = serial.Serial("/dev/tty.usbmodem144202", 115200, timeout=1)
 
-# baker tty.usbmodem144202
-startTime = datetime.now()
-# Start up arduino and make sure sensors are working properly
-fulldata = []
-print("Press reset on the arduino")
-# start
-while True:
-    if serZero.in_waiting:
-        zero_bytes = serZero.readline()
-        decoded_zero_bytes = zero_bytes.decode("utf-8")
-        decoded_zero_bytes = decoded_zero_bytes.strip()
-        print("Waiting for reset")
-        if decoded_zero_bytes == "Ready!":
-            break
-fulldata = []
-while True:
-    try:
+def collectData():
+    serZero = serial.Serial("/dev/tty.usbmodem14302", 115200, timeout=1)
+    startTime = datetime.now()
+    # Start up arduino and make sure sensors are working properly
+    fulldata = []
+    print("Press reset on the arduino")
+    # start
+    while True:
         if serZero.in_waiting:
             zero_bytes = serZero.readline()
             decoded_zero_bytes = zero_bytes.decode("utf-8")
             decoded_zero_bytes = decoded_zero_bytes.strip()
-            data = [float(x) for x in decoded_zero_bytes.split()]
-            fulldata.append(data)
-            print(decoded_zero_bytes)
-    except:
-        break
+            print("Waiting for reset")
+            if decoded_zero_bytes == "Ready!":
+                break
+    fulldata = []
+    while True:
+        try:
+            if serZero.in_waiting:
+                zero_bytes = serZero.readline()
+                decoded_zero_bytes = zero_bytes.decode("utf-8")
+                decoded_zero_bytes = decoded_zero_bytes.strip()
+                data = [float(x) for x in decoded_zero_bytes.split()]
+                fulldata.append(data)
+                print(decoded_zero_bytes)
+        except:
+            break
 
-endTime = datetime.now()
-total_t = endTime - startTime
-if abs(fulldata[0][0]) == 0.15:
-    del fulldata[0]
+    endTime = datetime.now()
+    return endTime, startTime, fulldata
 
-avg_base = np.zeros(15)
-for i in range(len(fulldata) - 1, -1, -1):
-    x = fulldata[i]
-    assert len(x) == 20
-    x = np.array(x)
-    for j in range(20, 0, -4):
-        x = np.delete(x, j - 1)
-    assert len(x) == 15
-    fulldata[i] = x
-    avg_base = avg_base + x
 
-avg_base = [x / len(fulldata) for x in avg_base]
-print(f"avg base num: {avg_base}")
+def save(startTime, endTime, fulldata, avg_base):
+    filename = f"{args.dataroot}{args.type}_data_batch{args.batchnum}.pkl"
+    infodict = {
+        "start_t": startTime.strftime("%Y-%m-%d %H-%M"),
+        "end_t": endTime.strftime("%Y-%m-%d %H-%M"),
+        "total_t": endTime - startTime,
+        "data": fulldata,
+        "avg_base": avg_base,
+        "type": args.type,
+    }
+    pickle.dump(infodict, open(filename, "wb"))
 
-avg_base = np.array(
-    [
-        -35.2282729805014,
-        -101.85376044568241,
-        -530.0596378830087,
-        -308.9774373259055,
-        284.7739554317551,
-        -1091.5754596100278,
-        -21.344707520891355,
-        -137.83245125348193,
-        -864.9162674094721,
-        -202.3215877437327,
-        -163.25055710306415,
-        -666.7459052924787,
-        -110.93732590529252,
-        -77.79108635097492,
-        -364.8731754874653,
-    ]
-)
-breakpoint()
-for i in range(len(fulldata)):
-    x = fulldata[i]
-    diff = x - avg_base
-    for j in range(len(diff)):
-        if abs(diff[j]) > abs(args.tol * avg_base[j]):
-            print(f"{i}, {abs(diff[j])}, {abs(args.tol * avg_base[j])}")
-            continue
 
-breakpoint()
-infodict = {
-    "start_t": startTime.strftime("%Y-%m-%d %H-%M"),
-    "end_t": endTime.strftime("%Y-%m-%d %H-%M"),
-    "total_t": total_t,
-    "data": fulldata,
-    "avg_base_used": avg_base,
-}
-pickle.dump(
-    infodict, open(f"{args.dataroot}base_data_batch{args.batchnum}.pkl", "wb")
-)
-print("DONE COLLECTING")
+def main():
+    endTime, startTime, fulldata = collectData()
 
-"""
-#get average abs sum signal for base, remove temp, check size of signal
-#replaced with avg vector...better 
-avg_base = 0
-for x in fulldata:
-    assert len(x) == 20
-    for i in range(20, 0, -4):
-        del x[i - 1]
-    assert len(x) == 15
-    abs_x = [abs(y) for y in x]
-    avg_base += sum(abs_x)
-avg_base = avg_base / len(fulldata)
-print(f"avg base num: {avg_base}")
-"""
+    # remove init data
+    if abs(fulldata[0][0]) == 0.15:
+        del fulldata[0]
+
+    if args.type == "base":
+        # remove temp, avg data, check size
+        avg_base = np.zeros(15)
+        for i in range(len(fulldata) - 1, -1, -1):
+            x = fulldata[i]
+            assert len(x) == 20
+            x = np.array(x)
+            for j in range(20, 0, -4):
+                x = np.delete(x, j - 1)
+            assert len(x) == 15
+            fulldata[i] = x
+            avg_base = avg_base + x
+        avg_base = [x / len(fulldata) for x in avg_base]
+        print(f"avg base num: {avg_base}")
+    else:
+        avg_base = []  # load base
+
+    save(startTime, endTime, fulldata, avg_base)
+    print("DONE COLLECTING")
+    breakpoint()
+
+
+if __name__ == "__main__":
+    main()
+
+    """
+    avg_base = np.array( [ -35.2282729805014,
+            -101.85376044568241,
+            -530.0596378830087,
+            -308.9774373259055,
+            284.7739554317551,
+            -1091.5754596100278,
+            -21.344707520891355,
+            -137.83245125348193,
+            -864.9162674094721,
+            -202.3215877437327,
+            -163.25055710306415,
+            -666.7459052924787,
+            -110.93732590529252,
+            -77.79108635097492,
+            -364.8731754874653,
+        ]
+    )
+
+    # checking for signal away from baseline
+    for i in range(len(fulldata)):
+        x = fulldata[i]
+        diff = x - avg_base
+        for j in range(len(diff)):
+            if abs(diff[j]) > abs(args.tol * avg_base[j]):
+                print(f"{i}, {abs(diff[j])}, {abs(args.tol * avg_base[j])}")
+                continue
+    """
+
+    """
+    #get average abs sum signal for base, remove temp, check size of signal
+    #replaced with avg vector...better 
+    avg_base = 0
+    for x in fulldata:
+        assert len(x) == 20
+        for i in range(20, 0, -4):
+            del x[i - 1]
+        assert len(x) == 15
+        abs_x = [abs(y) for y in x]
+        avg_base += sum(abs_x)
+    avg_base = avg_base / len(fulldata)
+    print(f"avg base num: {avg_base}")
+    """
 
